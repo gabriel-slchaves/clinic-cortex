25114bcc534fc3b5c6cc8b431017548cd5a01e7c
gabriel-slchaves <gabriel.slchaves@icloud.com>
2026-02-12 00:06:15 -0300

    front-end

---
+++ clinic_cortex/patients/mixins.py
@@ -0,0 +1,7 @@
+class ClinicQuerysetMixin:
+
+    def get_queryset(self):
+        return super().get_queryset().filter(
+            clinic=self.request.user.clinic,
+            is_active=True
+        )
--- clinic_cortex/patients/views.py
+++ clinic_cortex/patients/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .mixins import ClinicQuerysetMixin #Import Mixin
 
-# Create your views here.
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Patient
+
+
+class PatientListView(LoginRequiredMixin, ClinicQuerysetMixin, ListView):
+    model = Patient
+    template_name = 'patients/patients.html'
+    context_object_name = 'patients'
+    
+def patients(request):
+    return render(request, 'patients/patients.html')
